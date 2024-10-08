c6506d906b8e7b6daa5e5d527d3047dbe84316e4
Din9l <Stascarpov2101@yandex.ru>
2024-10-08 21:32:57 +0600

    1laba

---
+++ ConsoleAppMy/laboratornya_1.cs
@@ -0,0 +1,34 @@
+
+using System;
+
+// class one_exesize{
+//   static void Main() {
+//       int a,b;
+//       a=Convert.ToInt32(Console.ReadLine('Введите первое число: '));
+//       b=Convert.ToInt32(Console.ReadLine('Введите второе число: '));
+//       a=a+b;
+//       b=a-b;
+//       a=a-b;
+//     Console.WriteLine($"{a} {b}");
+//   }
+// }
+
+// class two_exesize{
+//   static void Main() {
+//       int x,y;
+//       x=Convert.ToInt32(Console.ReadLine('Введите первое число: '));
+//       y=Convert.ToInt32(Console.ReadLine('Введите второе число: '));
+//     Console.WriteLine(((Math.Abs(y+x)/2.0)+(Math.Abs(x-y))/2.0));
+//   }
+// }
+
+// class three_exesize{
+//   static void Main() {
+//       int l,m,p,n;
+//       n=Convert.ToInt32(Console.ReadLine('Введите количество грядок'));
+//       l=3;
+//       m=3;
+//       p=5;
+//     Console.WriteLine(2*n*p+n*(2*l+2*m+l*n-l));
+//   }
+// }
